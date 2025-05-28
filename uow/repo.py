36cc4a89c827565c31569fa4b1098177f5b6d9f8
Dmitry Naumov <danaumov@inno.tech>
2025-05-28 23:48:25 +0700

    фильтр

--- uow/repo.py
+++ uow/repo.py
@@ -1,32 +1,199 @@
-from sqlalchemy.ext.asyncio import AsyncSession
+import asyncio
+
+from api.v1.schemas import FilterPayload
+from core.repositories.alchemy.db import AsyncSessionLocal
+from core.repositories.alchemy.decorators import handle_db_errors
+from core.repositories.alchemy.filters.filter_engine import SqlAlchemyFilterEngine
+from core.repositories.alchemy.models import SqlThematic, SqlVoice, VoiceStatus
+from core.repositories.base_repository import BaseRepository
+from sqlalchemy import and_, func, select, update
+from sqlalchemy.exc import SQLAlchemyError
 
 
-class VoiceRepository:
-    def __init__(self, session: AsyncSession):
-        self.session = session
+class VoiceRepository(BaseRepository):
+    """
+    Репозиторий для работы с данными в базе данных с использованием SQLAlchemy.
+    """
 
-    async def update_status_by_ids(self, vote_ids: List[int], status: VoteStatus, author_id: Optional[int] = None):
-        await self.session.execute(
-            Vote.__table__.update().where(Vote.id.in_(vote_ids)).values(status=status.value, author_id=author_id)
+    def __init__(self, db: AsyncSessionLocal):
+        """
+        Инициализация репозитория.
+
+        Args:
+            db: Экземпляр асинхронной сессии базы данных.
+        """
+        self.db = db
+        self.voice_model = SqlVoice
+        self.thematic_model = SqlThematic
+        self.base_thematic_stmt = select(SqlThematic)
+        self.base_voice_stmt = select(SqlVoice)
+        self.active_thematic_stmt = self.base_thematic_stmt.where(self.thematic_model.is_active())
+        self.active_voice_stmt = self.base_voice_stmt.where(self.voice_model.is_active())
+
+    @handle_db_errors(default_return=[])
+    async def get_all_thematics(self) -> list[SqlThematic]:
+        """
+        Возвращает все категории из базы данных.
+
+        Returns:
+            Список всех категорий.
+        """
+        result = await self.db.execute(self.active_thematic_stmt)
+        return result.scalars().all()
+
+    @handle_db_errors(default_return=None)
+    async def get_thematic_by_id(self, _id: str) -> SqlThematic | None:
+        """
+        Возвращает тематику по её идентификатору.
+
+        Args:
+            _id: Идентификатор тематики.
+
+        Returns:
+            Объект тематики или None, если тематика не найдена.
+        """
+        stmt = self.active_thematic_stmt.where(self.thematic_model.get_ext_id() == _id)
+        result = await self.db.execute(stmt)
+        return result.scalar_one_or_none()
+
+    @handle_db_errors(default_return=[])
+    async def get_thematic_levels(self, ids: list[str]) -> list[dict]:
+        """
+        Возвращает уровни категорий по списку идентификаторов.
+
+        Args:
+            ids: Список идентификаторов категорий.
+
+        Returns:
+            Список словарей с уровнями категорий.
+        """
+        level_fields = [
+            (SqlThematic.level_1_id, SqlThematic.level_1_name),
+            (SqlThematic.level_2_id, SqlThematic.level_2_name),
+            (SqlThematic.level_3_id, SqlThematic.level_3_name),
+            (SqlThematic.level_4_id, SqlThematic.level_4_name),
+        ]
+
+        if ids is None or len(ids) > len(level_fields):
+            return []
+
+        level = len(ids)
+
+        id_field, name_field = level_fields[level]
+        columns = [
+            id_field.label("id"),
+            name_field.label("name"),
+        ]
+        if level == 4:
+            columns.append(SqlThematic.get_ext_id().label("thematic_id"))
+        stmt = self.active_thematic_stmt.with_only_columns(*columns).distinct(id_field)
+
+        for i, id_value in enumerate(ids):
+            stmt = stmt.where(level_fields[i][0] == id_value)
+
+        result = await self.db.execute(stmt)
+        return result.mappings().all()
+
+    @handle_db_errors(default_return=([], 0))
+    async def get_voices_with_paginates(
+        self, filter_payload: FilterPayload, user, limit: int, offset: int
+    ) -> tuple[list[SqlVoice], int]:
+        """
+        Возвращает список голосов и общее количество голосов, соответствующих фильтру.
+
+        Args:
+            filter_payload: Объект FilterPayload для фильтрации голосов
+            user: Объект пользователя
+            limit: Максимальное количество возвращаемых голосов
+            offset: Сдвиг для пагинации.
+
+        Returns:
+            Кортеж, содержащий список голосов и общее количество голосов.
+        """
+        filter_engine = SqlAlchemyFilterEngine(model=self.voice_model, white_list=self.voice_model.FILTER_WHITE_LIST)
+        stmt_voice = filter_engine.apply(filter_payload, user)
+        stmt_voice = stmt_voice.limit(limit).offset(offset)
+        stmt_voice_cnt = select(func.count()).select_from(filter_engine.apply(filter_payload, user).subquery())
+        voice_result, total_result = await asyncio.gather(
+            self.db.execute(stmt_voice),
+            self.db.execute(stmt_voice_cnt),
+        )
+        voices = voice_result.scalars().all()
+        total = total_result.scalar_one()
+
+        return voices, total
+
+    @handle_db_errors(default_return=([], 0))
+    async def get_voices(self, filter_payload: FilterPayload, user) -> list[SqlVoice]:
+        """
+        Возвращает список голосов соответствующих фильтру.
+
+        Args:
+            filter_payload: Объект FilterPayload для фильтрации голосов
+            user: Объект пользователя
+
+
+        Returns:
+            список голосов
+        """
+        filter_engine = SqlAlchemyFilterEngine(model=self.voice_model, white_list=self.voice_model.FILTER_WHITE_LIST)
+        stmt_voice = filter_engine.apply(filter_payload, user)
+
+        voice_result = await self.db.execute(stmt_voice)
+        voices = voice_result.scalars().all()
+        return voices
+
+    @handle_db_errors(default_return=None)
+    async def get_voice_by_id(self, _id: str) -> list[SqlVoice]:
+        stmt = self.active_voice_stmt.where(self.voice_model.get_voice_id() == _id)
+        res = await self.db.execute(stmt)
+        return res.mappings.all()
+
+    async def save_voices(self, voices: list[SqlVoice]) -> bool:
+        """
+        Сохраняет список голосов в базу данных.
+
+        Args:
+            voices: Список объектов SqlVoice для сохранения.
+
+        Returns:
+            True, если сохранение прошло успешно, иначе False.
+        """
+        try:
+            self.db.add_all(voices)
+            await self.db.commit()
+            return True
+        except SQLAlchemyError as e:
+            await self.db.rollback()
+            print(f"Error during bulk insert: {e}")
+            return False
+
+    async def annotate(self, record_ids: list[str], voice_ids: list[str], status: VoiceStatus):
+        stmt_record = (
+            update(self.voice_model).where(self.voice_model.get_record_id().in_(record_ids)).values(status=status.value)
+        )
+        stmt_voice = update(self.voice_model).where(
+            and_(self.voice_model.get_voice_id().in_(voice_ids), self.voice_model.get_record_id().not_in(record_ids))
+        )
+        await asyncio.gather(
+            *[
+                self.db.execute(stmt_record),
+                self.db.execute(stmt_voice),
+            ]
         )
 
-    async def update_groups_status(
-        self, vote_group_ids: List[int], status: VoteStatus, author_id: Optional[int] = None
-    ):
-        await self.session.execute(
-            Vote.__table__.update()
-            .where(Vote.vote_group_id.in_(vote_group_ids))
-            .values(status=status.value, author_id=author_id)
+    async def assign(self, voice_ids: list[str], status: VoiceStatus, user):
+        stmt = (
+            update(self.voice_model)
+            .where(self.voice_model.get_voice_id().in_(voice_ids))
+            .values(status=status.value, author_id=user.id)
         )
+        await self.db.execute(stmt)
 
-    async def update_groups_status_except_ids(
-        self,
-        vote_group_ids: List[int],
-        status: VoteStatus,
-        author_id: Optional[int] = None,
-        exclude_ids: List[int] = None,
-    ):
-        query = Vote.__table__.update().where(Vote.vote_group_id.in_(vote_group_ids))
-        if exclude_ids:
-            query = query.where(~Vote.id.in_(exclude_ids))
-        await self.session.execute(query.values(status=status.value, author_id=author_id))
+    async def unassign(self, voice_ids: list[str], status: VoiceStatus):
+        stmt = (
+            update(self.voice_model)
+            .where(self.voice_model.get_voice_id().in_(voice_ids))
+            .values(status=status.value, author_id=None)
+        )
+        await self.db.execute(stmt)
--- uow/voice_service.py
+++ uow/voice_service.py
@@ -1,10 +1,18 @@
+from conf.settings import settings
+from core.models.voice import ApiThematic, ApiThematicLevel, ApiVoice
+from core.repositories.alchemy.filters.filter_engine import FilterPayload
+from core.repositories.alchemy.models import VoiceStatus
+from core.repositories.alchemy.repository import VoiceRepository
+from core.repositories.alchemy.uow import UnitOfWork
+from fastapi import HTTPException
 from sqlalchemy.ext.asyncio import AsyncSession
-
-from uow.repo import VoiceRepository
-from uow.uow import UnitOfWork
 
 
 class VoiceService:
+    """
+    Сервис для управления голосами и тематиками.
+    """
+
     def __init__(self, session: AsyncSession | None = None, uow: UnitOfWork | None = None):
         if uow and session:
             raise ValueError("Передайте либо UnitOfWork, либо AsyncSession, но не оба")
@@ -14,44 +22,102 @@ class VoiceService:
         self.uow = uow
         self.voices = VoiceRepository(session) if session else None
 
-    async def validate_status_transition(self, current_statuses: List[VoteStatus], new_status: VoteStatus):
-        invalid_statuses = {VoteStatus.CONFIRMED, VoteStatus.CHANGED, VoteStatus.VIEWED}
-        if any(status in invalid_statuses for status in current_statuses):
-            raise HTTPException(
-                status_code=400, detail=f"Нельзя изменить статус записей с confirmed, changed или viewed"
-            )
-        if new_status == VoteStatus.CONFIRMED and any(status != VoteStatus.IN_PROGRESS for status in current_statuses):
-            raise HTTPException(status_code=400, detail="Подтвердить можно только голоса со статусом in_progress")
+    async def get_all_thematics(self) -> list[ApiThematic]:
+        """
+        Возвращает все тематики
+        в виде списка объектов ApiCategory.
 
-    async def change_status_by_filter(
-        self, filters: dict, new_status: VoteStatus, user_id: Optional[int] = None
-    ) -> None:
+        Returns:
+            Список объектов ApiCategory.
+        """
+        thematics = await self.voices.get_all_thematics()
+        return [ApiThematic.model_validate(th) for th in thematics]
+
+    async def get_thematic_by_id(self, _id) -> ApiThematic | None:
+        """
+        Возвращает тематику по ид
+
+        Returns:
+            Объект тематики или None, если тематика не найдена.
+        """
+        res = await self.voices.get_thematic_by_id(_id)
+        return ApiThematic.model_validate(res) if res is not None else None
+
+    async def get_thematic_levels(self, ids: list[str]) -> list[ApiThematicLevel]:
+        """
+        Возвращает уровни тематик по списку идентификаторов в виде списка объектов ApiCategoryLevel.
+
+        Args:
+            ids: Список идентификаторов уровней тематики.
+
+        Returns:
+            Список объектов ApiThematicLevel.
+        """
+        levels = await self.voices.get_thematic_levels(ids)
+        return [ApiThematicLevel.model_validate(level) for level in levels]
+
+    async def get_voices(
+        self, filter_payload: FilterPayload, user, limit: int, offset: int
+    ) -> tuple[list[ApiVoice], int]:
+        """
+        Возвращает список голосов и общее количество голосов, соответствующих фильтру, в виде кортежа.
+
+        Args:
+            filter_payload: Объект FilterPayload для фильтрации голосов
+            user: Объект пользователя
+            limit: Максимальное количество возвращаемых голосов
+            offset: Сдвиг для пагинации.
+
+        Returns:
+            Кортеж, содержащий список объектов ApiVoice и общее количество голосов.
+        """
+        voices, total = await self.voices.get_voices_with_paginates(filter_payload, user, limit, offset)
+        return [ApiVoice.model_validate(voice) for voice in voices], total
+
+    async def get_voice_by_id(self, _id: str) -> list[ApiVoice]:
+        """
+        Возвращает записи с одинаковым ид голоса
+
+        Args:
+            _id: voice_id записи
+        Returns:
+            Список объектов ApiVoice с одинаковым voice_id
+        """
+        voices = await self.voices.get_voice_by_id(_id)
+        return [ApiVoice.model_validate(voice) for voice in voices]
+
+    async def _validate_status_transition(self, current_statuses: list[VoiceStatus], new_status: VoiceStatus):
+        transition_status_rules = {
+            VoiceStatus.NEW: (lambda x: x == VoiceStatus.IN_PROGRESS, "IN_PROGRESS => NEW"),
+            VoiceStatus.IN_PROGRESS: (lambda x: x == VoiceStatus.NEW, "NEW => IN_PROGRESS"),
+            VoiceStatus.CHANGED: (lambda x: x == VoiceStatus.IN_PROGRESS, "IN_PROGRESS => CHANGED"),
+            VoiceStatus.CONFIRMED: (lambda x: x == VoiceStatus.IN_PROGRESS, "IN_PROGRESS => CONFIRMED"),
+            VoiceStatus.VIEWED: (lambda x: x == VoiceStatus.IN_PROGRESS, "IN_PROGRESS => VIEWED"),
+        }
+        rule, msg = transition_status_rules[new_status]
+        if not all(rule(status) for status in current_statuses):
+            raise HTTPException(status_code=400, detail=f"Для выбранных голосов разрешено только {msg}")
+
+    async def change_status_by_filter(self, filter_payload: FilterPayload, new_status: VoiceStatus, user) -> None:
         async with self.uow() as uow:
-            votes = await uow.votes.get_by_filters(filters)
-            if not votes:
+            voices = await uow.voices.get_voices(filter_payload, user)
+            if not voices:
                 raise HTTPException(status_code=404, detail="Голоса по фильтру не найдены")
 
-            current_statuses = [VoteStatus(vote.status) for vote in votes]
-            await self.validate_status_transition(current_statuses, new_status)
+            current_statuses = [VoiceStatus(vote.status) for vote in voices]
+            await self._validate_status_transition(current_statuses, new_status)
 
-            vote_group_ids = [vote.vote_group_id for vote in votes]
-            vote_ids = [vote.id for vote in votes]
+            voice_ids = [voice.get_voice_id() for voice in voices]
+            record_ids = [voice.get_record_id() for voice in voices]
 
-            batch_size = 10000
-            for i in range(0, len(vote_group_ids), batch_size):
-                batch_group_ids = vote_group_ids[i : i + batch_size]
-                batch_vote_ids = vote_ids[i : i + batch_size]
+            batch_size = settings.db.voice_count_for_update
+            for i in range(0, len(voice_ids), batch_size):
+                batch_voice_ids = voice_ids[i : i + batch_size]
+                batch_record_ids = record_ids[i : i + batch_size]
 
-                if new_status == VoteStatus.IN_PROGRESS:
-                    if user_id is None:
-                        raise HTTPException(status_code=400, detail="Для статуса in_progress требуется user_id")
-                    await uow.votes.update_groups_status(batch_group_ids, VoteStatus.IN_PROGRESS, user_id)
-                elif new_status in [VoteStatus.CONFIRMED, VoteStatus.CHANGED]:
-                    await uow.votes.update_status_by_ids(batch_vote_ids, new_status)
-                    await uow.votes.update_groups_status_except_ids(
-                        batch_group_ids, VoteStatus.VIEWED, None, batch_vote_ids
-                    )
-                elif new_status == VoteStatus.NEW:
-                    await uow.votes.update_groups_status(batch_group_ids, VoteStatus.NEW, None)
-                else:
-                    await uow.votes.update_status_by_ids(batch_vote_ids, new_status)
+                if new_status == VoiceStatus.IN_PROGRESS:
+                    await uow.voices.assign(batch_voice_ids, VoiceStatus.IN_PROGRESS, user)
+                elif new_status in [VoiceStatus.CONFIRMED, VoiceStatus.CHANGED]:
+                    await uow.voices.annotate(batch_record_ids, batch_voice_ids, new_status)
+                elif new_status == VoiceStatus.NEW:
+                    await uow.voices.unassign(batch_voice_ids, VoiceStatus.NEW)
